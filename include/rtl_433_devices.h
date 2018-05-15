48728ce61c349a41cb6d50d680b84d92303579cd
Daniel Krueger <palstek@users.noreply.github.com>
2018-05-15 21:53:42 +0200

    Add support for Bresser Weather Center 5-in-1
    Signed-off-by: Daniel Krueger <palstek@users.noreply.github.com>

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -104,7 +104,8 @@
 		DECL(ge_coloreffects) \
 		DECL(x10_sec) \
 		DECL(interlogix) \
-		DECL(dish_remote_6_3)
+		DECL(dish_remote_6_3) \
+		DECL(bresser_5in1)
 
 typedef struct {
 	char name[256];
